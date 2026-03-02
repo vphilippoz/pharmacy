aa62631b845db4b98ae66ca3309cb7e89e35b29c
V. Philippoz <v.philippoz@hublot.ch>
2026-03-02 10:35:51 +0100

    Peripheral module working on Wokwi

--- Software/src/peripherals/peripherals.ino
+++ Software/src/peripherals/peripherals.ino
@@ -18,13 +18,39 @@ void setup() {
     }
 
     // Setup the library
-    backend::setup(VERBOSE);
+    peripherals::setup(VERBOSE);
     if(VERBOSE) {Serial.println("Setup complete");}
 }
 
 void loop() {
-    // Test module functionality here
+    // Declare variables
+    static uint8_t brightness = 0;
+    static std::vector<std::vector<uint16_t>> frame = {
+        {0xFFFF, 0x0000, 0xFFFF, 0x0000, 0xFFFF, 0x0000, 0xFFFF, 0x0000}, // Top panel
+        {0x0000, 0xFFFF, 0x0000, 0xFFFF, 0x0000, 0xFFFF, 0x0000, 0xFFFF}, // Bottom panel
+        {0xAAAA, 0x5555, 0xAAAA, 0x5555, 0xAAAA, 0x5555, 0xAAAA, 0x5555}}; // Center panel
+    
+    // Update the LED matrices
+    peripherals::set_brightness(brightness);
+    peripherals::set_frame(frame);
 
 
-    delay(1e4); // Wait 10 seconds
+    // Print infos
+    if(VERBOSE) {
+        Serial.print("Brightness: ");
+        Serial.println(peripherals::get_brightness());
+        Serial.print("Next animation required: ");
+        Serial.println(peripherals::get_next_animation_required() ? "Yes" : "No");
+        // Serial.print("Current frame: ");
+        // peripherals::print_frame(frame);
+    }
+    
+    // Update
+    brightness = (brightness + 1) % 16; // Increment brightness level (0-15)
+    for (size_t i = 0; i < frame.size(); i++) {
+        for (size_t j = 0; j < frame[i].size(); j++) {
+            frame[i][j] = ~frame[i][j]; // Invert the bits to create a new pattern
+        }
+    }
+    delay(500); // [ms]
 }
